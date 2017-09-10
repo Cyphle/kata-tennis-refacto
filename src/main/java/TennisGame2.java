ac995413c426613abbb99dc2154e9b9409fd5c5c
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-10 18:00:39 +0200

    Delete one condition

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -16,37 +16,28 @@ public class TennisGame2 implements TennisGame {
 
   public String getScore() {
     String score = "";
-    if (P1point == P2point && P1point < 4) {
+    if (P1point == P2point) {
       score = Score.findScoreByPoint(P1point);
       score += "-All";
     }
-    if (P1point == P2point && P1point >= 3)
-      score = "Deuce";
-
-    if (P1point > 0 && P2point == 0) {
+    if ((P1point > 0 && P2point == 0) || P2point > 0 && P1point == 0) {
       P1res = Score.findScoreByPoint(P1point);
-      P2res = "Love";
-      score = P1res + "-" + P2res;
-    }
-
-    if (P2point > 0 && P1point == 0) {
       P2res = Score.findScoreByPoint(P2point);
-
-      P1res = "Love";
       score = P1res + "-" + P2res;
     }
-
     if (P1point > P2point && P1point < 4) {
       P1res = Score.findScoreByPoint(P1point);
       P2res = Score.findScoreByPoint(P2point);
       score = P1res + "-" + P2res;
     }
     if (P2point > P1point && P2point < 4) {
-      P2res = Score.findScoreByPoint(P2point);
       P1res = Score.findScoreByPoint(P1point);
+      P2res = Score.findScoreByPoint(P2point);
       score = P1res + "-" + P2res;
     }
 
+    if (P1point == P2point && P1point >= 3)
+      score = "Deuce";
     if (P1point > P2point && P2point >= 3) {
       score = "Advantage player1";
     }
