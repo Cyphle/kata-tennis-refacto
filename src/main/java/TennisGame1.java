21ce80ccdf460ae2da96c108062e3d7021355f73
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-10 16:21:02 +0200

    Remove variable score

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -22,19 +22,25 @@ public class TennisGame1 implements TennisGame {
   public String getScore() {
     if (doesPlayersHaveSameScore()) return Score.findByPointForSamePoint(m_score1);
 
-    String score = "";
     if (m_score1 >= 4 || m_score2 >= 4) {
-      int minusResult = m_score1 - m_score2;
-      if (minusResult == 1) score = "Advantage player1";
-      else if (minusResult == -1) score = "Advantage player2";
-      else if (minusResult >= 2) score = "Win for player1";
-      else score = "Win for player2";
+      int pointDifference = m_score1 - m_score2;
+
+      if (pointDifference == 1) {
+        return "Advantage player1";
+      }
+
+      if (pointDifference == -1) {
+        return "Advantage player2";
+      }
+
+      if (pointDifference >= 2) {
+        return "Win for player1";
+      }
+
+      return "Win for player2";
     } else {
-      score += Score.findByPoint(m_score1);
-      score += "-";
-      score += Score.findByPoint(m_score2);
+      return Score.findByPoint(m_score1) + "-" + Score.findByPoint(m_score2);
     }
-    return score;
   }
 
   private boolean doesPlayersHaveSameScore() {
