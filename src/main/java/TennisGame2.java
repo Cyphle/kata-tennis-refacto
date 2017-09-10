3799498b90b57fec29a00d3f5a64916c17c6097d
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-10 18:28:18 +0200

    Renaming

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,73 +1,55 @@
 import tennisgametwo.Score;
 
 public class TennisGame2 implements TennisGame {
-  public int P1point = 0;
-  public int P2point = 0;
-
-  public String P1res = "";
-  public String P2res = "";
-  private String player1Name;
-  private String player2Name;
-
-  public TennisGame2(String player1Name, String player2Name) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
+  private int P1point = 0;
+  private int P2point = 0;
 
   public String getScore() {
-    if (P1point == P2point) {
-      if (P1point >= 3)
+    if (playersHaveSameScore()) {
+      if (scoreIsHigherThanThreePoints())
         return "Deuce";
       else
         return Score.findScoreByPoint(P1point) + "-All";
     }
 
-    if (P1point > P2point && P1point < 4 || P2point > P1point && P2point < 4)
+    if (gameIsNotFinished())
       return Score.findScoreByPoint(P1point) + "-" + Score.findScoreByPoint(P2point);
 
     String score = "";
-    if (P1point > P2point && P2point >= 3) {
-      score = "Advantage player1";
-    }
-    if (P2point > P1point && P1point >= 3) {
-      score = "Advantage player2";
+    if (P1point > P2point) {
+      if (P2point >= 3) score = "Advantage player1";
+      if ((P1point - P2point) >= 2) score = "Win for player1";
     }
 
-    if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-      score = "Win for player1";
-    }
-    if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
-      score = "Win for player2";
+    if (P2point > P1point) {
+      if (P1point >= 3) score = "Advantage player2";
+      if ((P2point - P1point) >= 2) score = "Win for player2";
     }
     return score;
   }
 
-  public void SetP1Score(int number) {
-
-    for (int i = 0; i < number; i++) {
-      P1Score();
-    }
-
+  private boolean gameIsNotFinished() {
+    return P1point > P2point && P1point < 4 || P2point > P1point && P2point < 4;
   }
 
-  public void SetP2Score(int number) {
-
-    for (int i = 0; i < number; i++) {
-      P2Score();
-    }
-
+  private boolean scoreIsHigherThanThreePoints() {
+    return P1point >= 3;
   }
 
-  public void P1Score() {
+  private boolean playersHaveSameScore() {
+    return P1point == P2point;
+  }
+
+  private void P1Score() {
     P1point++;
   }
 
-  public void P2Score() {
+  private void P2Score() {
     P2point++;
   }
 
   public void wonPoint(String player) {
-    if (player == "player1")
+    if (player.equals("player1"))
       P1Score();
     else
       P2Score();
